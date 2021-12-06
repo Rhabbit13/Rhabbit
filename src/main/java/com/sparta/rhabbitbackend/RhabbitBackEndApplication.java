37a9b7367ad27f04f6d6ec59f835fad503a3a5d8
good4y <kmosh1212@naver.com>
2021-12-06 21:13:24 +0900

    first

---
+++ src/main/java/com/sparta/rhabbitbackend/RhabbitBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.rhabbitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhabbitBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhabbitBackEndApplication.class, args);
+    }
+
+}
